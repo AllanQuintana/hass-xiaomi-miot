8b1a758b84153e8291a0810e2724d45e5d72f884
AllanQuintana <115678472+AllanQuintana@users.noreply.github.com>
2023-04-10 12:15:19 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,129 +1,105 @@
 #!/bin/bash
-# wget -q -O - https://raw.githubusercontent.com/al-one/hass-xiaomi-miot/master/install.sh | bash -
-# wget -q -O - https://raw.githubusercontent.com/al-one/hass-xiaomi-miot/master/install.sh | ARCHIVE_TAG=v1.0.0 bash -
-# wget -q -O - https://raw.githubusercontent.com/al-one/hass-xiaomi-miot/master/install.sh | HUB_DOMAIN=hub.fastgit.xyz bash -
-# wget -q -O - https://raw.githubusercontent.com/al-one/hass-xiaomi-miot/master/install.sh | DOMAIN=hacs REPO_PATH=hacs-china/integration ARCHIVE_TAG=main bash -
 set -e
 
+# Default values
 [ -z "$DOMAIN" ] && DOMAIN="xiaomi_miot"
 [ -z "$REPO_PATH" ] && REPO_PATH="al-one/hass-xiaomi-miot"
 REPO_NAME=$(basename "$REPO_PATH")
-
 [ -z "$ARCHIVE_TAG" ] && ARCHIVE_TAG="$1"
 [ -z "$ARCHIVE_TAG" ] && ARCHIVE_TAG="master"
 [ -z "$HUB_DOMAIN" ] && HUB_DOMAIN="github.com"
-ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/archive/$ARCHIVE_TAG.zip"
 
-if [ "$ARCHIVE_TAG" = "latest" ]; then
-    ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/releases/$ARCHIVE_TAG/download/$DOMAIN.zip"
-fi
-if [ "$DOMAIN" = "hacs" ]; then
-    if [ "$ARCHIVE_TAG" = "main" ] || [ "$ARCHIVE_TAG" = "china" ]; then
-        ARCHIVE_TAG="latest"
-    fi
-    ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/releases/$ARCHIVE_TAG/download/$DOMAIN.zip"
-fi
-
+# Colors for output
 RED_COLOR='\033[0;31m'
 GREEN_COLOR='\033[0;32m'
 GREEN_YELLOW='\033[1;33m'
 NO_COLOR='\033[0m'
 
-declare haPath
-declare ccPath
-declare -a paths=(
+# Functions
+info() { echo -e "${GREEN_COLOR}INFO: $1${NO_COLOR}"; }
+warn() { echo -e "${GREEN_YELLOW}WARN: $1${NO_COLOR}"; }
+error() { echo -e "${RED_COLOR}ERROR: $1${NO_COLOR}"; [ "$2" != "false" ] && exit 1; }
+
+checkRequirement() {
+  if [ -z "$(command -v "$1")" ]; then
+    error "'$1' is not installed"
+  fi
+}
+
+findConfigPath() {
+  declare -a paths=(
     "$PWD"
     "$PWD/config"
     "/config"
     "$HOME/.homeassistant"
     "/usr/share/hassio/homeassistant"
-)
+  )
 
-function info () { echo -e "${GREEN_COLOR}INFO: $1${NO_COLOR}";}
-function warn () { echo -e "${GREEN_YELLOW}WARN: $1${NO_COLOR}";}
-function error () { echo -e "${RED_COLOR}ERROR: $1${NO_COLOR}"; if [ "$2" != "false" ]; then exit 1;fi; }
-
-function checkRequirement () {
-    if [ -z "$(command -v "$1")" ]; then
-        error "'$1' is not installed"
+  for path in "${paths[@]}"; do
+    if [ -f "$path/home-assistant.log" ]; then
+      haPath="$path"
+      break
+    elif [ -d "$path/.storage" ] && [ -f "$path/configuration.yaml" ]; then
+      haPath="$path"
+      break
     fi
+  done
 }
 
+# Main script
 checkRequirement "wget"
 checkRequirement "unzip"
 
-info "Archive URL: $ARCHIVE_URL"
-info "Trying to find the correct directory..."
-for path in "${paths[@]}"; do
-    if [ -n "$haPath" ]; then
-        break
-    fi
-
-    if [ -f "$path/home-assistant.log" ]; then
-        haPath="$path"
-    else
-        if [ -d "$path/.storage" ] && [ -f "$path/configuration.yaml" ]; then
-            haPath="$path"
-        fi
-    fi
-done
+findConfigPath
 
 if [ -n "$haPath" ]; then
-    info "Found Home Assistant configuration directory at '$haPath'"
-    cd "$haPath" || error "Could not change path to $haPath"
-    ccPath="$haPath/custom_components"
-    if [ ! -d "$ccPath" ]; then
-        info "Creating custom_components directory..."
-        mkdir "$ccPath"
-    fi
+  info "Found Home Assistant configuration directory at '$haPath'"
+  cd "$haPath" || error "Could not change path to $haPath"
 
-    info "Changing to the custom_components directory..."
-    cd "$ccPath" || error "Could not change path to $ccPath"
+  # Determine archive URL
+  if [ "$ARCHIVE_TAG" = "latest" ]; then
+    ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/releases/$ARCHIVE_TAG/download/$DOMAIN.zip"
+  elif [ "$DOMAIN" = "hacs" ] && ([ "$ARCHIVE_TAG" = "main" ] || [ "$ARCHIVE_TAG" = "china" ]); then
+    ARCHIVE_TAG="latest"
+    ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/releases/$ARCHIVE_TAG/download/$DOMAIN.zip"
+  else
+    ARCHIVE_URL="https://$HUB_DOMAIN/$REPO_PATH/archive/$ARCHIVE_TAG.zip"
+  fi
+  
+  # Download and install
+info "Archive URL: $ARCHIVE_URL"
+info "Changing to the custom_components directory..."
+custom_components_path="$haPath/custom_components"
+mkdir -p "$custom_components_path"
+cd "$custom_components_path" || error "Could not change path to $custom_components_path"
+info "Downloading..."
+wget -t 2 -O "$custom_components_path/$ARCHIVE_TAG.zip" "$ARCHIVE_URL"
 
-    info "Downloading..."
-    wget -t 2 -O "$ccPath/$ARCHIVE_TAG.zip" "$ARCHIVE_URL"
+info "Unpacking..."
+unzip -o "$custom_components_path/$ARCHIVE_TAG.zip" -d "$custom_components_path" >/dev/null 2>&1
+repo_dir="$custom_components_path/$REPO_NAME-$ARCHIVE_TAG"
 
-    if [ -d "$ccPath/$DOMAIN" ]; then
-        warn "custom_components/$DOMAIN directory already exist, cleaning up..."
-        rm -R "$ccPath/$DOMAIN"
-    fi
+if [ -d "$custom_components_path/$DOMAIN" ]; then
+    warn "custom_components/$DOMAIN directory already exists, cleaning up..."
+    rm -R "$custom_components_path/$DOMAIN"
+fi
 
-    ver=${ARCHIVE_TAG/#v/}
-    if [ ! -d "$ccPath/$REPO_NAME-$ver" ]; then
-        ver=$ARCHIVE_TAG
-    fi
+if [ -d "$repo_dir/custom_components/$DOMAIN" ]; then
+    info "Copying files..."
+    cp -r "$repo_dir/custom_components/$DOMAIN" "$custom_components_path"
+else
+    error "Could not find custom_components/$DOMAIN directory in the archive" false
+fi
 
-    info "Unpacking..."
-    str="/releases/"
-    if [ ${ARCHIVE_URL/${str}/} = $ARCHIVE_URL ]; then
-        unzip -o "$ccPath/$ARCHIVE_TAG.zip" -d "$ccPath" >/dev/null 2>&1
-    else
-        dir="$ccPath/$REPO_NAME-$ver/custom_components/$DOMAIN"
-        mkdir -p $dir
-        unzip -o "$ccPath/$ARCHIVE_TAG.zip" -d $dir >/dev/null 2>&1
-    fi
-
-    if [ ! -d "$ccPath/$REPO_NAME-$ver" ]; then
-        error "Could not find $REPO_NAME-$ver directory" false
-        error "找不到文件夹: $REPO_NAME-$ver"
-    fi
-    cp -rf "$ccPath/$REPO_NAME-$ver/custom_components/$DOMAIN" "$ccPath"
-
-    info "Removing temp files..."
-    rm -rf "$ccPath/$ARCHIVE_TAG.zip"
-    rm -rf "$ccPath/$REPO_NAME-$ver"
-    info "Installation complete."
-    info "安装成功！"
-    echo
-    info "Remember to restart Home Assistant before you configure it."
-    info "请重启 Home Assistant"
+info "Removing temporary files..."
+rm -rf "$custom_components_path/$ARCHIVE_TAG.zip"
+rm -rf "$repo_dir"
+info "Installation complete."
+info "Remember to restart Home Assistant before you configure it."
 
 else
-    echo
-    error "Could not find the directory for Home Assistant" false
-    error "找不到 Home Assistant 根目录" false
-    echo "Manually change the directory to the root of your Home Assistant configuration"
-    echo "With the user that is running Home Assistant and run the script again"
-    echo "请手动进入 Home Assistant 根目录后再次执行此脚本"
-    exit 1
+  error "Could not find the directory for Home Assistant" false
+  echo "Manually change the directory to the root of your Home Assistant configuration"
+  echo "With the user that is running Home Assistant and run the script again"
+  exit 1
 fi
